9a16ea390c5954af933e322609eecc7f63e7f3a6
Naman Talreja <namantalreja2704@gmail.com>
2024-06-28 00:34:35 +0530

    added login sign up

---
+++ Competifit/Core/Authentication/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  Competifit
+//
+//  Created by Naman Talreja on 27/06/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
