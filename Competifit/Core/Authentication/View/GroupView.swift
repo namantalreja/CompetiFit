b1b17d065f7f62c90bc982f698e1cce5c6e6d726
Naman Talreja <namantalreja2704@gmail.com>
2024-06-30 00:37:24 +0530

    added some group features and corrected schema

---
+++ Competifit/Core/Authentication/View/GroupView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupView.swift
+//  Competifit
+//
+//  Created by Naman Talreja on 29/06/24.
+//
+
+import SwiftUI
+
+struct GroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupView()
+}
